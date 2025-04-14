a11e7800a92ec493ac7584be8a699215873f748e
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-14 16:48:27 -0300

    Remove a palavra-chave 'required' das propriedades Name e Duration na classe Service

--- Models/Service.cs
+++ Models/Service.cs
@@ -9,11 +9,11 @@ namespace SchedulingSystemAPI.Models
 
         [Required]
         [StringLength(100)]
-        public required string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required]
         [Range(1, 999)]
-        public required int Duration { get; set; }
+        public int Duration { get; set; }
 
         [StringLength(500)]
         public string? Description { get; set; }
