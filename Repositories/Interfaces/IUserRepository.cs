4330af3daf2c03739efd1084baa0740f78d70741
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-14 16:11:28 -0300

    Atualiza IUserRepository e UserRepository para incluir suporte à exclusão permanente de usuários e filtragem de usuários inativos na recuperação.

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -4,12 +4,13 @@ namespace SchedulingSystemAPI.Repositories.Interfaces
 {
     public interface IUserRepository
     {
-        Task<IEnumerable<User>> GetAllAsync();
+        Task<IEnumerable<User>> GetAllAsync(bool includeInactive = false);
         Task<User?> GetByIdAsync(int id);
         Task<User?> GetByEmailAsync(string email);
         Task AddAsync(User user);
         Task UpdateAsync(User user);
         Task DeleteAsync(int id);
+        Task PermanentDeleteAsync(int id);
     }
 }
 
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -9,9 +9,12 @@ namespace SchedulingSystemAPI.Repositories
     {
         private readonly AppDbContext _context = context;
 
-        public async Task<IEnumerable<User>> GetAllAsync()
+        public async Task<IEnumerable<User>> GetAllAsync(bool includeInactive = false)
         {
-            return await _context.Users.ToListAsync();
+            if (includeInactive)
+                return await _context.Users.ToListAsync();
+            else
+                return await _context.Users.Where(u => u.IsActive).ToListAsync();
         }
 
         public async Task<User?> GetByIdAsync(int id)
@@ -37,6 +40,16 @@ namespace SchedulingSystemAPI.Repositories
         }
 
         public async Task DeleteAsync(int id)
+        {
+            var user = await GetByIdAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task PermanentDeleteAsync(int id)
         {
             var user = await GetByIdAsync(id);
             if (user != null)
